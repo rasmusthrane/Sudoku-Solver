6dd63a52e17718e3da9d194aa357e919e0ca3c5a
rasmusthrane <re.thrane@hotmail.dk>
2025-09-23 12:47:41 +0000

    renamed squares to cells

--- src/main/standard/SmallSudokuGame.py
+++ src/main/standard/SmallSudokuGame.py
@@ -8,30 +8,36 @@ class SmallSudokuGame(FormalGameInterface):
     def __init__(self) -> None:
         self.cols = "123"
         self.rows = "ABC"
-        self.squares = cross(self.rows, self.cols)
+        self.cells = cross(self.rows, self.cols)
         self.unitlist = ([cross(self.rows, self.cols)]) # Only one unit in small sudoku 
 
         # Create a dict that holds all units that each square belongs to
         self.units: Dict[str, List[List[str]]] = {}
-        for s in self.squares:
+        for c in self.cells:
             units_for_s: List[List[str]] = []  # List to hold units containing s
             for u in self.unitlist:
-                if s in u:
+                if c in u:
                     units_for_s.append(u)
-            self.units[s] = units_for_s
+            self.units[c] = units_for_s
 
-        # Create a dict that holds all squares which shares unit with a square
+        # Create a dict that holds all cells which shares unit with a square
         self.peers: Dict[str, Set[str]] = {}
-        for s in self.squares:
-            all_squares: List[str] = [] # Flattened list to hold all squares that share unit with s
-            for unit in self.units[s]:
-                all_squares.extend(unit)
-            peers_of_s: Set[str] = set(all_squares) - set([s])
-            self.peers[s] = peers_of_s
+        for c in self.cells:
+            all_cells: List[str] = [] # Flattened list to hold all cells that share unit with s
+            for unit in self.units[c]:
+                all_cells.extend(unit)
+            peers_of_c: Set[str] = set(all_cells) - set([c])
+            self.peers[c] = peers_of_c
         
         self.nrows = len(self.rows)
         self.ncols = len(self.cols)
         self.nsubgrids = 1 
+        self.ncells = len(self.cells)
+
+        # Initialize grid
+        #print(self.cells)
+        grid = '.'*self.ncells
+        print(grid)
 
     def getWinStatus(self) -> Literal["win", "ongoing"]:
         return "win"
