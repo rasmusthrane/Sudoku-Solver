cf21d8a873c1ee7be1076a229acbac24d6d1367e
rasmusthrane <re.thrane@hotmail.dk>
2025-09-24 14:02:53 +0000

    added a test helper func for highligthing prints

--- src/testing/utility/TestHelper.py
+++ src/testing/utility/TestHelper.py
@@ -6,4 +6,18 @@ class TestHelper:
     def printGameState(game: FormalGameInterface):
         print("=== Game State Print ===")
 
+    @staticmethod
+    def printHighlight(any: object):
+        RED = "\033[91m"
+        BOLD = "\033[1m"
+        RESET = "\033[0m"
+
+        print(f"{BOLD}{RED}{'='*20}")
+        print(any)
+        print(f"{'='*20}{RESET}")
+
+
+if __name__ == "__main__":
+    testHelper = TestHelper()
+    testHelper.printHighlight("hi")
 
