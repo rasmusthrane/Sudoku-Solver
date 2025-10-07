85240d98363d847c6ed7d51bd8bd7294dd3c2c17
rasmusthrane <re.thrane@hotmail.dk>
2025-10-07 09:57:15 +0000

    implemented computing unit list for 4 by 4 sudoku

--- src/main/variants/sudokuBoardStrategy/Sudoku4by4.py
+++ src/main/variants/sudokuBoardStrategy/Sudoku4by4.py
@@ -1,5 +1,7 @@
 from main.variants.sudokuBoardStrategy.SudokuBoardStrategy import SudokuBoardStrategy
 
+from typing import List
+
 #class Sudoku3by3(SudokuBoardStrategy):
 class Sudoku4by4(SudokuBoardStrategy):
     def __init__(self, clues:str) -> None:
@@ -16,3 +18,17 @@ class Sudoku4by4(SudokuBoardStrategy):
     
     def getNumberOfSubGrids(self) -> int:
         return 4
+    
+    def getUnitList(self) -> List[List[str]]:
+        rows: str = self.getRows()
+        cols: str = self.getCols()
+
+        row_units = [[r + c for c in cols] for r in rows]
+        col_units = [[r + c for r in rows] for c in cols]
+
+        row_groups = [rows[i:i+2] for i in range(0, 4, 2)]
+        col_groups = [cols[i:i+2] for i in range(0, 4, 2)]
+        box_units = [[r + c for r in rg for c in cg] for rg in row_groups for cg in col_groups]
+
+        unitlist = row_units + col_units + box_units
+        return unitlist
