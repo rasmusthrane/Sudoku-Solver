ebebceeec94a0c67cbdae0680bf27e07fb686643
rasmusthrane <re.thrane@hotmail.dk>
2025-10-20 13:18:40 +0000

    can now see a 4x4 sudok board and set clues

--- src/main/app/__init__.py
+++ src/main/app/__init__.py
@@ -1,19 +1,25 @@
 from main.standard.SquareSudokuGame import SquareSudokuGame
 from main.variants.factory.Factory4by4 import Factory4by4
+from main.standard.GameConstants import GameConstants
 
 from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
 
-    game = SquareSudokuGame(Factory4by4())
-
+    clues = '...3............'
+    game = SquareSudokuGame(Factory4by4(clues=clues))
+    grid_value_dict = game.getGridValueDict()
     @app.route('/hello')
     def hello(): # type: ignore
-        return f'Hello, World! And the game state is: {game.getGameState()}'
+        return f'Hello, World! And the game state is: {game.cols}'
     
     @app.route('/')
     def index(): # type: ignore
-        return render_template("template.html")
+        return render_template("template.html", 
+                               grid_value_dict=grid_value_dict, 
+                               row_letters=game.rows, 
+                               col_numbers=game.cols,
+                               EMPTY_CELL = GameConstants.EMPTY_CELL)
 
     return app
