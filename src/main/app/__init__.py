d30d41102b494cc6029258a0dd12266e96933ef4
rasmusthrane <re.thrane@hotmail.dk>
2025-10-20 14:37:56 +0000

    began trying to set numbers in the GUI but template.html is getting bloated

--- src/main/app/__init__.py
+++ src/main/app/__init__.py
@@ -1,8 +1,10 @@
 from main.standard.SquareSudokuGame import SquareSudokuGame
-from main.variants.factory.Factory4by4 import Factory4by4
 from main.standard.GameConstants import GameConstants
+from main.framework.status import Status
+from main.variants.factory.Factory4by4 import Factory4by4
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+
 
 def create_app():
     app = Flask(__name__)
@@ -12,7 +14,7 @@ def create_app():
     grid_value_dict = game.getGridValueDict()
     @app.route('/hello')
     def hello(): # type: ignore
-        return f'Hello, World! And the game state is: {game.cols}'
+        return f'Hello, World! And the game state is: {grid_value_dict}'
     
     @app.route('/')
     def index(): # type: ignore
@@ -21,5 +23,17 @@ def create_app():
                                row_letters=game.rows, 
                                col_numbers=game.cols,
                                EMPTY_CELL = GameConstants.EMPTY_CELL)
+    @app.route('/update_cell', methods=['POST'])
+    def update_cell(): #type: ignore
+        data = request.get_json()
+        cell = data.get('cell')
+        value = data.get('value')
+        # Attempt to update the game
+        status = game.setCellValue(cell, value)
+        if status == Status.OK:
+            return jsonify(success=True)
+        else:
+            return jsonify(success=False, error=str(status))
+
 
     return app
