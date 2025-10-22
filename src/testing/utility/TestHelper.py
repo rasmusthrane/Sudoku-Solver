e364ebcd336eece91e02098d80b60fbb4d367da0
rasmusthrane <re.thrane@hotmail.dk>
2025-10-22 14:13:21 +0000

    test: add helper function to TestHelper for cleaner tests code

--- src/testing/utility/TestHelper.py
+++ src/testing/utility/TestHelper.py
@@ -1,4 +1,12 @@
+from main.app import create_app
 from main.framework.game import FormalGameInterface
+
+from typing import Tuple
+from flask import Flask
+from flask.testing import FlaskClient
+
+from main.standard.SquareSudokuGame import SquareSudokuGame
+from main.variants.factory.Factory4by4 import Factory4by4
 
 class TestHelper:
 
@@ -15,6 +23,16 @@ class TestHelper:
         print(f"{BOLD}{RED}{'='*20}")
         print(any)
         print(f"{'='*20}{RESET}")
+
+    @staticmethod
+    def createAndInjectCluedGameIntoAppForTesting(clues: str) -> Tuple[SquareSudokuGame, Flask, FlaskClient]:
+        game = SquareSudokuGame(Factory4by4(clues=clues))
+        app: Flask = create_app(game)
+        app.testing = True
+        client: FlaskClient = app.test_client() # create a test client
+        return game, app, client
+
+
 
 
 if __name__ == "__main__":
