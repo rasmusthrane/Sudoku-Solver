abd999f575e34e74fd339eb0bd8fed948e55486f
rasmusthrane <re.thrane@hotmail.dk>
2025-10-29 10:02:49 +0000

    refactor: rename  to

--- src/main/variants/factory/Factory3by3.py
+++ src/main/variants/factory/Factory3by3.py
@@ -1,6 +1,6 @@
 from main.variants.factory.GameFactory import GameFactory
 from main.variants.sudokuBoardStrategy.sudoku_board_strategy import SudokuBoardStrategy
-from main.variants.sudokuBoardStrategy.Sudoku3by3 import Sudoku3by3
+from main.variants.sudokuBoardStrategy.sudoku_3x3 import Sudoku3by3
 
 class Factory3by3(GameFactory):
     def __init__(self, clues:str = '.........') -> None:
