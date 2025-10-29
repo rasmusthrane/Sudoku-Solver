62cbf69df0f8aaafc63c50e42fda37544cf4966b
rasmusthrane <re.thrane@hotmail.dk>
2025-10-29 10:03:51 +0000

    refactor: rename `Sudoky4by4.py` to `sudoku_4x4.py`

--- src/main/variants/factory/Factory4by4.py
+++ src/main/variants/factory/Factory4by4.py
@@ -1,6 +1,6 @@
 from main.variants.factory.GameFactory import GameFactory
 from main.variants.sudokuBoardStrategy.sudoku_board_strategy import SudokuBoardStrategy
-from main.variants.sudokuBoardStrategy.Sudoku4by4 import Sudoku4by4
+from main.variants.sudokuBoardStrategy.sudoku_4x4 import Sudoku4by4
 
 class Factory4by4(GameFactory):
     def __init__(self, clues:str = '................') -> None:
